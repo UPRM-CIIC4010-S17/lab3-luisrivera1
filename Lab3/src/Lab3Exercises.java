6a57418894d24bca6ce061bdc07a2aef54ac1039
Luis A.Rivera Marquez <luis.rivera79@upr.edu>
2017-02-08 17:28:23 -0400

    Made minor changes to the JFrame's location and close operation method.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Luis A. Rivera Marquez");
+		JFrame myFrame = new JFrame("Luis Rivera Marquez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(630, 300);
+		myFrame.setLocation(635, 305);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
